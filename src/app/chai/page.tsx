a5bb6b8153d00384c6abf80389fd59ae06bd5287
rajx7722 <rajx7722@users.noreply.github.com>
2024-10-03 19:26:40 +0530

    HeroSection added, working on FeaturedCourses

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,5 @@
+export default function rajpage(){
+    return(
+        <h1 className="text-7xl text-center text-red-600 bg-white ">hello there raj!</h1>
+    )
+}
---
+++ src/components/FeaturedCourses.tsx
@@ -0,0 +1,13 @@
+import courseData from "../data/music-courses.json"
+
+function FeaturedCourses() {
+  return (
+    <div className="py-12 bg-yellow-300">
+      <div className=" bg-black">1</div>
+      <div className=" bg-black">2</div>
+      <div className=" bg-black">3</div>
+    </div>
+  )
+}
+
+export default FeaturedCourses
