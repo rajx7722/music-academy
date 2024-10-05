2243ae53d977e0c34d27c21e7b70bc42264e79bb
rajx7722 <rajx7722@users.noreply.github.com>
2024-10-06 02:25:00 +0530

    added footer and wavy background section- Meet our Instructors

--- src/components/UpcomingWebinars.tsx
+++ src/components/UpcomingWebinars.tsx
@@ -1,6 +1,8 @@
+'use client'
 import Link from "next/link";
-function UpcomingWebinars() {
+import { HoverEffect } from "./ui/CardHoverEffecr";
 
+function UpcomingWebinars() {
     const featuredWebinars = [
         {
           title: 'Understanding Music Theory',
@@ -47,7 +49,6 @@ function UpcomingWebinars() {
         },
       ];
     
-
   return (
     <div className="p-12 bg-gray-900 ">
      <div className=" max-w-7xl mx-auto px-4 sm:px-6">
@@ -55,7 +56,17 @@ function UpcomingWebinars() {
             <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">FEATURED WEBINARS</h2>
             <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">Enhance your Musical Journey</p>
         </div>
-        <div className="mt-10">2</div>
+        <div className="mt-10">
+        <HoverEffect
+          items={featuredWebinars.map(webinar => (
+            {
+              title: webinar.title,
+              description: webinar.description,
+              link: '/'
+            }
+          ))}
+          />
+        </div>
         <div className="text-center mt-10">
             <Link href={"/"} className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200">
             View All Webinars
