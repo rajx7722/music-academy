5051dd6d55fa8fdb657e83f17386f22b321cbd0f
rajx7722 <rajx7722@users.noreply.github.com>
2024-10-06 03:34:06 +0530

    footer added , courses page added , images left to download and add to the public folder in courses and will have to make a contact page .

--- src/app/chai/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function rajpage(){
-    return(
-        <h1 className="text-7xl text-center text-red-600 bg-white ">hello there raj!</h1>
-    )
-}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+
+
+function page() {
+  return (
+    <div>
+      test 2
+
+    </div>
+  )
+}
+
+export default page
