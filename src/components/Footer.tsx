b06f9540b6bd402701021ebe16f1692ca56eca57
rajx7722 <rajx7722@users.noreply.github.com>
2024-10-09 02:09:40 +0530

    site 100% working and done. Added contact us page with a beams background and the images which were left

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -74,8 +74,8 @@ function Footer() {
           </div>
           <div>
             <h2 className="text-white text-lg font-semibold mb-4">Contact Us</h2>
-            <p>New Delhi, India</p>
-            <p>Delhi 10001</p>
+            <p>bangalore, India</p>
+            <p>Bangalore 560068</p>
             <p>Email: info@musicschool.com</p>
             <p>Phone: (123) 456-7890</p>
           </div>
